7e4e7f09306ccec9433224466c0d96cf1d203f76
maida-fl <maida.guzman@outlook.com>
2022-04-24 16:08:42 -0300

    burger nav links animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,20 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.burger-nav');
+    const navLinks = document.querySelectorAll('.burger-nav li');
     
     burger.addEventListener('click', () => {
+        //Toggle Nav
         nav.classList.toggle('burger-nav-active');
-    })
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.2}s`;
+            }
+        });
+    });
 }
 
 navSlide();
