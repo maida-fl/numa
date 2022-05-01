2406702dfd472a58c9df77ddcb1c7bc9b5485eb2
maida-fl <maida.guzman@outlook.com>
2022-05-01 19:51:26 -0300

    burger nav search bar transitions styling and categories titles

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,7 @@ const navSlide = () => {
             if(link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.2}s`;
+                link.style.animation = `navLinkFade 0.6s ease forwards ${index / 12 + 0.1}s`;
             }
         });
         //Animate Burger 
